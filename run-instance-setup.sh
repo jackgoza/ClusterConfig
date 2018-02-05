122752bf5350465faf5a282dc82a3334b99dad3b
anask <katib87@gmail.com>
2018-02-05 12:29:24 -0600

    Add command to check setup logs

--- run-instance-setup.sh
+++ run-instance-setup.sh
@@ -106,7 +106,9 @@ while read ADDRESS ; do
      fi
 done < $ADDRESSES
 
-echo "WAITING FOR SETUP TO FINISH.."
+echo -e "\nRUN THE FOLLOWING COMMAND IN ANOTHER WINDOW TO CHECK THE STATUS OF EACH NODE:\n-------------------\nwhile read m; do echo -n $m: && ssh $m 'tail -n 1 setup-log.txt' < /dev/null ; done < cluster-machines.txt \n-------------------"
+
+echo -e "\nWAITING FOR SETUP TO FINISH.."
 wait
 
 
